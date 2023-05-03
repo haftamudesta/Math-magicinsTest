40fb9c05beeab42028bf38846867b473176412a2
haftamudesta <desta451616@hotmail.com>
2023-05-03 23:39:02 +0300

    Fix linter errors

--- src/__tests__/calculate.test.jsx
+++ src/__tests__/calculate.test.jsx
@@ -2,31 +2,45 @@ import calculate from '../logic/calculate';
 
 describe('Check calculate function behavior according to the operation', () => {
   test('Sum operation button', () => {
-    expect(calculate({ total: '500', next: '200', operation: '-' }, '+')).toEqual({ total: '300', next: null, operation: '+' });
+    expect(
+      calculate({ total: '500', next: '200', operation: '-' }, '+')
+    ).toEqual({ total: '300', next: null, operation: '+' });
   });
 
   test('Substraction operation button', () => {
-    expect(calculate({ total: '50', next: '25', operation: '-' }, 'x')).toEqual({ total: '25', next: null, operation: 'x' });
+    expect(calculate({ total: '50', next: '25', operation: '-' }, 'x')).toEqual(
+      { total: '25', next: null, operation: 'x' }
+    );
   });
 
   test('Devide operation button', () => {
-    expect(calculate({ total: '700', next: '2', operation: '÷' }, '=')).toEqual({ total: '350', next: null, operation: null });
+    expect(calculate({ total: '700', next: '2', operation: '÷' }, '=')).toEqual(
+      { total: '350', next: null, operation: null }
+    );
   });
 
   test('+/- operation button', () => {
-    expect(calculate({ total: 85, next: null, operation: null }, '+/-')).toEqual({ total: '-85', next: null, operation: null });
+    expect(
+      calculate({ total: 85, next: null, operation: null }, '+/-')
+    ).toEqual({ total: '-85', next: null, operation: null });
   });
 
   test('% operation button', () => {
-    expect(calculate({ total: null, next: '800', operation: null }, '%')).toEqual({ total: '800', next: null, operation: '%' });
+    expect(
+      calculate({ total: null, next: '800', operation: null }, '%')
+    ).toEqual({ total: '800', next: null, operation: '%' });
   });
 
   test('Equal result after a Multiply operation', () => {
-    expect(calculate({ total: '30', next: '97', operation: 'x' }, '=')).toEqual({ total: '2910', next: null, operation: null });
+    expect(calculate({ total: '30', next: '97', operation: 'x' }, '=')).toEqual(
+      { total: '2910', next: null, operation: null }
+    );
   });
 
   test('AC reset operation button', () => {
-    expect(calculate({ total: '216', next: '80', operation: null }, 'AC')).toEqual({ total: null, next: null, operation: null });
+    expect(
+      calculate({ total: '216', next: '80', operation: null }, 'AC')
+    ).toEqual({ total: null, next: null, operation: null });
   });
 
   test('Add dot to an operation', () => {
